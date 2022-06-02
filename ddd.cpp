24e1eb03ee8829c84b6d8c7476558f0bb20a7732
Deepak Dhakad <deepakd08032000@gmail.com>
2022-06-02 23:14:53 +0530

    last

--- ddd.cpp
+++ ddd.cpp
@@ -3,7 +3,13 @@ using namespace std;
 int main(){
 
     int a=8, b=6 ,c;
-    
+    cout<<"enter the value";
+    cout<<"enter the value";
+    cout<<"enter the value";
+    cout<<"enter the value";
+    cout<<"enter the value";
+    cout<<"enter the value";
+    cout<<"enter the value";
     c =a+b;
     cout<<"the sum is"<<c<<endl;
     return 0;
